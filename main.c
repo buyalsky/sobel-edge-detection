21241d913b5464bfb61cca822d976f0cd47c872d
burak <burak.dursunlar@hotmail.com>
2020-10-21 17:36:15 +0300

    add binary pgm support

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdbool.h>
 
-typedef struct Image{
+typedef struct{
     int width;
     int height;
     int **content;
@@ -21,6 +21,71 @@ const int yAxis[3][3] = {
         {-1, -2, -1}
 };
 
+void readBinaryPgm(Image *sourceImage, FILE *fptr){
+    char buffer[100];
+    fgets(buffer, 100, fptr);
+    if (buffer[0] == '#')
+        fgets(buffer, 100, fptr);
+    char *suffix;
+    char *token = strtok(buffer, " ");
+    sourceImage->width = (int) strtol(token, &suffix, 10);
+
+    token = strtok(NULL, " ");
+    sourceImage->height = (int) strtol(token, &suffix, 10);
+    fgets(buffer, 100, fptr);
+
+    unsigned char *plainImage = (unsigned char *) malloc(sourceImage->width *
+            sourceImage->height * sizeof(unsigned char));
+
+    fread(plainImage, sizeof(unsigned char), sourceImage->width * sourceImage->height, fptr);
+
+    fgets(buffer, 10000, fptr);
+
+    sourceImage->content = (int **) malloc(sourceImage->height * sizeof(int *));
+
+    for (int i = 0; i < sourceImage->height; ++i) {
+        sourceImage->content[i] = (int *) malloc(sourceImage->width * sizeof(int));
+        for (int j = 0; j < sourceImage->width; ++j) {
+            sourceImage->content[i][j] = plainImage[i * sourceImage->width + j];
+        }
+    }
+}
+
+void readAsciiPgm(Image *sourceImage, FILE *fptr){
+    char buffer[100];
+    fgets(buffer, 100, fptr);
+    if (buffer[0] == '#')
+        fgets(buffer, 100, fptr);
+    char *suffix;
+    char *token = strtok(buffer, " ");
+    sourceImage->width = (int) strtol(token, &suffix, 10);
+
+    token = strtok(NULL, " ");
+    sourceImage->height = (int) strtol(token, &suffix, 10);
+
+    sourceImage->content = (int **) malloc(sourceImage->height * sizeof(int *));
+
+    for (int i = 0; i < sourceImage->height; ++i) {
+        sourceImage->content[i] = (int *) malloc(sourceImage->width * sizeof(int));
+        int j = 0;
+        char value[4] = "\0";
+        int c = getc(fptr);
+        bool enteredInt = false;
+        while (j < sourceImage->width){
+            if (c>=48 && c<=57){
+                strncat(value, (const char *) &c, 1);
+                enteredInt = true;
+            } else if (enteredInt){
+                sourceImage->content[i][j] = (int) strtol(value, &suffix, 10);
+                strcpy(value, "");
+                enteredInt = false;
+                j++;
+            }
+            c = getc(fptr);
+        }
+    }
+}
+
 void writeImageToFile(Image image, char *filename){
     FILE *file;
     if ((file = fopen(filename, "w")) == NULL){
@@ -37,7 +102,7 @@ void writeImageToFile(Image image, char *filename){
                 fprintf(file, "   %d", image.content[i][j]);
             else if (image.content[i][j]<100 && image.content[i][j]>=10)
                 fprintf(file, "  %d", image.content[i][j]);
-            else if (image.content[i][j]>=100 && image.content[i][j] <= 255)
+            else if (image.content[i][j]>=100 && image.content[i][j]<=255)
                 fprintf(file, " %d", image.content[i][j]);
             else
                 fprintf(file, " 255");
@@ -47,26 +112,46 @@ void writeImageToFile(Image image, char *filename){
     fclose(file);
 }
 
-void normalizeMatrix(int **matrice, int width, int height){
-    int min = matrice[0][0], max = matrice[0][0];
-    for (int i = 0; i < height; ++i) {
-        for (int j = 0; j < width; ++j) {
-            if (min > matrice[i][j]) min = matrice[i][j];
-            if (max < matrice[i][j]) max = matrice[i][j];
+void writeImageToFileB(Image image, char *filename){
+    FILE *file;
+    if ((file = fopen(filename, "wb")) == NULL){
+        perror("Error happened while opening file!");
+        exit(1);
+    }
+
+    fprintf(file, "P5\n%d %d\n255\n", image.width, image.height);
+    for (int i = 0; i < image.height; ++i) {
+        for (int j = 0; j < image.width; ++j) {
+            fprintf(file, "%c", image.content[i][j]);
+            /*if (image.content[i][j] < 0)
+                fprintf(file, "%c", 0);
+            else if (image.content[i][j] > 255)
+                fprintf(file, "%c", 255);
+            else
+                fprintf(file, "%c", image.content[i][j]);*/
+
+        }
+    }
+}
+
+void normalizeMatrix(Image *image){
+    int min = image->content[0][0], max = image->content[0][0];
+    for (int i = 0; i < image->height; ++i) {
+        for (int j = 0; j < image->width; ++j) {
+            if (min > image->content[i][j]) min = image->content[i][j];
+            if (max < image->content[i][j]) max = image->content[i][j];
         }
     }
 
     int difference = max - min;
 
-    for (int i = 0; i < height; ++i) {
-        for (int j = 0; j < width; ++j) {
+    for (int i = 0; i < image->height; ++i) {
+        for (int j = 0; j < image->width; ++j) {
             if (difference == 0)
-                matrice[i][j] = 255;
+                image->content[i][j] = 255;
             else{
-                int result = 255*(matrice[i][j] - min)/(difference);
-                printf("%d\n", result);
-                matrice[i][j] = result;
-
+                int result = 255 * (image->content[i][j] - min) / (difference);
+                image->content[i][j] = result;
             }
         }
     }
@@ -96,7 +181,7 @@ int filterByY(int **frame){
            yAxis[2][2] * frame[2][2];
 }
 
-int ***extractMatrixIntoFramesX(Image image){
+int ***extractMatrixIntoFrames(Image image){
     int frameArrayHeight = image.height - 2;
     int frameArrayWidth = image.width - 2;
     int ***frameArray = (int ***) malloc(frameArrayHeight * frameArrayWidth * sizeof(int **));
@@ -129,55 +214,27 @@ int ***extractMatrixIntoFramesX(Image image){
 }
 
 int main() {
-    char arr[100];
+    char buffer[5];
     FILE *fptr;
+    Image sourceImage, outImageX, outImageY;
+    sourceImage.content = NULL;
+
     if ((fptr = fopen("lena.ascii.pgm", "r")) == NULL){
         perror("Error happened while opening file!");
         exit(1);
     }
 
-    fgets(arr, 100, fptr);
-    fgets(arr, 100, fptr);
-    if (arr[0] == '#')
-        fgets(arr, 100, fptr);
-    Image sourceImage, outImageX, outImageY;
-    char * suffix;
-    char * token = strtok(arr, " ");
-    sourceImage.width = (int) strtol(token, &suffix, 10);
+    fgets(buffer, 5, fptr);
 
-    token = strtok(NULL, " ");
-    sourceImage.height = (int) strtol(token, &suffix, 10);
-
-    fgets(arr, 10000, fptr);
-
-    sourceImage.content = (int **) malloc(sourceImage.height * sizeof(int *));
-
-    for (int i = 0; i < sourceImage.height; ++i) {
-        sourceImage.content[i] = (int *) malloc(sourceImage.width * sizeof(int));
-    }
-
-    for (int i = 0; i < sourceImage.height; ++i) {
-        int j = 0;
-        char value[4] = "\0";
-        int c = getc(fptr);
-        bool enteredInt = false;
-        while (j < sourceImage.width){
-            if (c>=48 && c<=57){
-                strncat(value, (const char *) &c, 1);
-                enteredInt = true;
-            } else if (enteredInt){
-                sourceImage.content[i][j] = (int) strtol(value, &suffix, 10);
-                strcpy(value, "");
-                enteredInt = false;
-                j++;
-            }
-            c = getc(fptr);
-        }
-    }
-
+    if (buffer[1] == '2')
+        readAsciiPgm(&sourceImage, fptr);
+    else
+        readBinaryPgm(&sourceImage, fptr);
     fclose(fptr);
+    normalizeMatrix(&sourceImage);
+    writeImageToFileB(sourceImage, "imageOutB.pgm");
 
-    int ***frameArray = extractMatrixIntoFramesX(sourceImage);
+    int ***frameArray = extractMatrixIntoFrames(sourceImage);
 
     outImageX.width = sourceImage.width - 2;
     outImageX.height = sourceImage.height - 2;
@@ -189,7 +246,9 @@ int main() {
             outImageX.content[i][j] = filterByX(frameArray[i * outImageX.width + j]);
         }
     }
+    normalizeMatrix(&outImageX);
     writeImageToFile(outImageX, "imageOutX.pgm");
+    writeImageToFileB(outImageX, "imageOutXB.pgm");
 
     outImageY.width = sourceImage.width - 2;
     outImageY.height = sourceImage.height - 2;
@@ -202,7 +261,10 @@ int main() {
             outImageY.content[i][j] = filterByY(frameArray[i * outImageY.width + j]);
         }
     }
+    normalizeMatrix(&outImageY);
     writeImageToFile(outImageY, "imageOutY.pgm");
+    writeImageToFileB(outImageY, "imageOutYB.pgm");
+
 
     return 0;
 }
