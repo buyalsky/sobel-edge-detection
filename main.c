4492f385642d8c7a62c10716617ea84a336296b4
burak <burak.dursunlar@hotmail.com>
2020-10-26 23:39:17 +0300

    add parser

--- main.c
+++ main.c
@@ -1,9 +1,31 @@
+#include <getopt.h>
+#include <string.h>
+#include <stdlib.h>
+
 #include "pgm_lib.h"
 
 
-int main() {
+int main(int argc, char *argv[]) {
+    int opt;
+    char outputPath[20], inputPath[20];
     Image sourceImage, outImageX, outImageY;
-    readPgmFile(&sourceImage, "lena2.pgm", "rb");
+    static char usage[] = "usage: %s -x [input file] -o [output file]\n";
+
+    while ((opt = getopt(argc, argv, "x:o:")) != -1){
+        switch (opt) {
+            case 'o':
+                strcpy(outputPath, optarg);
+                break;
+            case 'x':
+                strcpy(inputPath, optarg);
+                break;
+            default:
+                fprintf(stderr, usage, argv[0]);
+                exit(1);
+        }
+    }
+
+    readPgmFile(&sourceImage, inputPath, "rb");
 
     normalizeImage(&sourceImage);
     sourceImage = addPaddingToImage(sourceImage);
@@ -14,11 +36,10 @@ int main() {
     Image outImage = mergeImages(&outImageX, &outImageY);
 
     normalizeImage(&outImageX);
-    writeImage(outImageX, "imageOutX.pgm");
     normalizeImage(&outImageY);
-    writeImage(outImageY, "imageOutY.pgm");
     normalizeImage(&outImage);
-    writeImage(outImage, "imageOut.pgm");
+
+    writeImage(outImage, outputPath);
 
     return 0;
 }
